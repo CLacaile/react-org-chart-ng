728d85f78c2e75061e8c7f387d4644dad6166aee
CLacaile <clementlacaile@gmail.com>
2025-01-04 16:34:44 +0100

    Went ahead with the Organization implementation

--- src/components/Organization.tsx
+++ src/components/Organization.tsx
@@ -1,21 +1,53 @@
-import { LIGHTBLUE } from "../utils/palette";
-import { ORG_PADDING } from "../utils/constants";
+import { DARKGREY, WHITE } from "../utils/palette";
+import TextWrap from "./TextWrap";
+import { motion } from "framer-motion";
 
 interface OrganizationProps {
+  text: string;
+  x: number;
+  y: number;
   width: number;
   height: number;
   minWidth?: number;
+  onClick?: () => void;
 }
 
-function Organization({ width, height, minWidth = width }: OrganizationProps) {
+function Organization({
+  text,
+  x,
+  y,
+  width,
+  height,
+  minWidth = width,
+  onClick,
+}: OrganizationProps) {
   return (
-    <rect
-      width={width < minWidth ? minWidth : width}
-      height={height}
-      rx="5"
-      fill={LIGHTBLUE}
-      transform={`translate(${-width / 2}, ${-ORG_PADDING})`}
-    />
+    <motion.g
+      className="organization"
+      initial={{ x, y, filter: "none" }}
+      whileHover={{
+        x: x - 1,
+        y: y - 1,
+        filter: "url(#softShadow)",
+      }}
+      transition={{ duration: 0.3 }}
+      onClick={onClick}
+    >
+      <rect
+        width={width < minWidth ? minWidth : width}
+        height={height}
+        rx="5"
+        fill={WHITE}
+        stroke={DARKGREY}
+        style={{ cursor: "pointer" }}
+      />
+      <TextWrap
+        position={{ x: width / 2, y: 15 }}
+        maxWidth={200}
+        text={text}
+        weight="bold"
+      />
+    </motion.g>
   );
 }
 
